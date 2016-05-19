d23a600425c54a8ac8fe0ad8653090bd947ff394
lth2015 <18910648861@163.com>
2016-05-19 21:34:16 +0800

    modidfy push-mysql-endpoint.json

---
+++ kubernetes-elasticsearch-cluster/es-down.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+kubectl delete rc es-client1 
+kubectl delete rc es-client2 
+kubectl delete rc es-client3 
+kubectl delete rc es-data    
+kubectl delete rc es-data1   
+kubectl delete rc es-data2   
+kubectl delete rc es-data3   
+kubectl delete rc es-data4   
+kubectl delete rc es-master1 
+kubectl delete rc es-master2 
+kubectl delete rc es-master3 
