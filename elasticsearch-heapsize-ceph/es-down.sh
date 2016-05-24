6cccc30afdd365c0bb402fa8408a632be9a59f98
lth2015 <18910648861@163.com>
2016-05-24 16:48:57 +0800

    add serviceaccount

--- elasticsearch-heapsize-ceph/es-down.sh
+++ elasticsearch-heapsize-ceph/es-down.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 kubectl delete rc es-client1 
 kubectl delete rc es-client2 
 kubectl delete rc es-client3 
@@ -10,3 +11,9 @@ kubectl delete rc es-data4
 kubectl delete rc es-master1 
 kubectl delete rc es-master2 
 kubectl delete rc es-master3 
+
+
+kubectl delete -f service-account.json
+
+kubectl delete -f es-discovery-svc.json
+kubectl delete -f es-svc.json
--- elasticsearch-heapsize-ceph/es-up.sh
+++ elasticsearch-heapsize-ceph/es-up.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# kubectl create -f service-account.json
-# 
-# kubectl create -f es-discovery-svc.json
-# kubectl create -f es-svc.json
 
-kubectl create -f es-client1-rc-heapsize.json  --validate=false
-kubectl create -f es-client2-rc-heapsize.json  --validate=false
-kubectl create -f es-client3-rc-heapsize.json  --validate=false
-kubectl create -f es-data1-rc-heapsize.json    --validate=false
-kubectl create -f es-data2-rc-heapsize.json    --validate=false
-kubectl create -f es-data3-rc-heapsize.json    --validate=false
-kubectl create -f es-data4-rc-heapsize.json    --validate=false
-kubectl create -f es-master1-rc-heapsize.json  --validate=false
-kubectl create -f es-master2-rc-heapsize.json  --validate=false
-kubectl create -f es-master3-rc-heapsize.json  --validate=false
+kubectl create -f service-account.json
+kubectl create -f es-discovery-svc.json
+kubectl create -f es-svc.json
+
+kubectl create -f es-client1-rc-heapsize.json   --validate=false
+kubectl create -f es-client2-rc-heapsize.json   --validate=false
+kubectl create -f es-client3-rc-heapsize.json   --validate=false
+kubectl create -f es-data1-rc-heapsize.json     --validate=false
+kubectl create -f es-data2-rc-heapsize.json     --validate=false
+kubectl create -f es-data3-rc-heapsize.json     --validate=false
+kubectl create -f es-data4-rc-heapsize.json     --validate=false
+kubectl create -f es-master1-rc-heapsize.json   --validate=false
+kubectl create -f es-master2-rc-heapsize.json   --validate=false
+kubectl create -f es-master3-rc-heapsize.json   --validate=false
