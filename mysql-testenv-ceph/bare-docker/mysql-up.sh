be3343586975829b5dbea563627d140f37a68f0d
Administrator <admin@example.com>
2016-08-15 13:28:18 +0800

    add mysql-up.sh mysql-down.sh

--- mysql-testenv-ceph/bare-docker/mysql-up.sh
+++ mysql-testenv-ceph/bare-docker/mysql-up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -tid --name mysql-yce -e "MYSQL_ROOT_PASSWORD=root" -p 3306:3306 -v data:/data mysql:latest
+docker run -tid --name mysql-yce -e "MYSQL_ROOT_PASSWORD=root" -p 3306:3306 -v $PWD/data:/var/lib/mysql  mysql:latest
