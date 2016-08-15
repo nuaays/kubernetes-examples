fb9ecca843548515ca4589aebfaa9d7243028132
lth2015 <lth9379@gmail.com>
2016-08-15 17:34:14 +0800

    add --net=host

--- mysql-testenv-ceph/bare-docker/mysql-up.sh
+++ mysql-testenv-ceph/bare-docker/mysql-up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -tid --name mysql-yce -e "MYSQL_ROOT_PASSWORD=root" -p 3306:3306 -v $PWD/data:/var/lib/mysql  mysql:latest
+docker run -tid --name mysql-yce --net=host -e "MYSQL_ROOT_PASSWORD=root" -p 3306:3306 -v $PWD/data:/var/lib/mysql registry.test.com:5000/mysql:latest
