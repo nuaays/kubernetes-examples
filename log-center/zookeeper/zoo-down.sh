be3c1e805a135b9e6b34fce7db3bf6eb82f3cf9c
18910648861@163.com <18910648861@163.com>
2017-05-09 16:02:05 +0800

    add pod.json

--- log-center/zookeeper/zoo-down.sh
+++ log-center/zookeeper/zoo-down.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-kubectl delete -f zookeeper-1-rc.json
+kubectl delete -f zookeeper-1-pod.json
 kubectl delete -f zookeeper-1-svc.json
 
-kubectl delete -f zookeeper-2-rc.json
+kubectl delete -f zookeeper-2-pod.json
 kubectl delete -f zookeeper-2-svc.json
 
-kubectl delete -f zookeeper-3-rc.json
+kubectl delete -f zookeeper-3-pod.json
 kubectl delete -f zookeeper-3-svc.json
 
-kubectl delete -f zookeeper-4-rc.json
+kubectl delete -f zookeeper-4-pod.json
 kubectl delete -f zookeeper-4-svc.json
 
-kubectl delete -f zookeeper-5-rc.json
+kubectl delete -f zookeeper-5-pod.json
 kubectl delete -f zookeeper-5-svc.json
--- log-center/zookeeper/zoo-up.sh
+++ log-center/zookeeper/zoo-up.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-kubectl create -f zookeeper-1-rc.json --validate=false
+kubectl create -f zookeeper-1-pod.json --validate=false
 kubectl create -f zookeeper-1-svc.json
 
-kubectl create -f zookeeper-2-rc.json --validate=false
+kubectl create -f zookeeper-2-pod.json --validate=false
 kubectl create -f zookeeper-2-svc.json
 
-kubectl create -f zookeeper-3-rc.json --validate=false
+kubectl create -f zookeeper-3-pod.json --validate=false
 kubectl create -f zookeeper-3-svc.json
 
-kubectl create -f zookeeper-4-rc.json --validate=false
+kubectl create -f zookeeper-4-pod.json --validate=false
 kubectl create -f zookeeper-4-svc.json
 
-kubectl create -f zookeeper-5-rc.json --validate=false
+kubectl create -f zookeeper-5-pod.json --validate=false
 kubectl create -f zookeeper-5-svc.json
